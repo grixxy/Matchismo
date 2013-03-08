ef1f10b0cab799c720e7eb7516ea31bd70a80564
Gregory Shukhter <grixxy@gmail.com>
2013-03-07 22:44:40 -0800

    commit1

---
+++ Matchismo/Model/Deck.h
@@ -0,0 +1,15 @@
+//
+//  Deck.h
+//  Matchismo
+//
+//  Created by Gregory on 3/7/13.
+//  Copyright (c) 2013 Gregory. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Card.h"
+
+@interface Deck : NSObject
+- (void)addCard:(Card *)card atTop:(BOOL)atTop;
+- (Card *)drawRandomCard;
+@end
---
+++ Matchismo/Model/Deck.m
@@ -0,0 +1,46 @@
+//
+//  Deck.m
+//  Matchismo
+//
+//  Created by Gregory on 3/7/13.
+//  Copyright (c) 2013 Gregory. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface Deck()
+@property (strong, nonatomic) NSMutableArray *cards; // of Card
+@end
+
+@implementation Deck
+
+- (NSMutableArray *)cards
+{
+    if (!_cards) _cards = [[NSMutableArray alloc] init];
+    return _cards;
+}
+
+
+- (Card *)drawRandomCard
+{
+    Card *randomCard = nil;
+    if (self.cards.count) {
+        unsigned index = arc4random() % self.cards.count;
+        randomCard = self.cards[index];
+        [self.cards removeObjectAtIndex:index];
+    }
+    return randomCard;
+}
+
+- (void)addCard:(Card *)card atTop:(BOOL)atTop
+{
+    if (atTop) {
+        [self.cards insertObject:card atIndex:0];
+    } else {
+        [self.cards addObject:card];
+    }
+}
+
+
+
+@end
