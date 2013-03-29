b54ec662dcb3f7e20418b844587773db5b3dbffa
Gregory Shukhter <grixxy@gmail.com>
2013-03-28 18:17:14 -0700

    add new class

---
+++ Matchismo/SetGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetGameViewController.h
+//  Matchismo
+//
+//  Created by Gregory on 3/28/13.
+//  Copyright (c) 2013 Gregory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetGameViewController : UIViewController
+
+@end
