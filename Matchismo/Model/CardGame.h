7d62ac09e10ff5bbf485266299505ab3e59039b0
Gregory Shukhter <grixxy@gmail.com>
2013-04-05 18:18:08 -0700

    t

---
+++ Matchismo/Model/CardGame.h
@@ -0,0 +1,13 @@
+//
+//  CardGame.h
+//  Matchismo
+//
+//  Created by Gregory on 3/31/13.
+//  Copyright (c) 2013 Gregory. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface CardGame : Deck
+
+@end
---
+++ Matchismo/Model/CardGame.m
@@ -0,0 +1,13 @@
+//
+//  CardGame.m
+//  Matchismo
+//
+//  Created by Gregory on 3/31/13.
+//  Copyright (c) 2013 Gregory. All rights reserved.
+//
+
+#import "CardGame.h"
+
+@implementation CardGame
+
+@end
