3453fdaf69030b3c4189d731f3afd0106ef7308b
Gregory Shukhter <grixxy@Gregorys-MacBook-Pro.local>
2013-04-06 16:08:02 -0700

    add new files

---
+++ Matchismo/GameResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameResultViewController.h
+//  Matchismo
+//
+//  Created by Gregory Shukhter on 4/6/13.
+//  Copyright (c) 2013 Gregory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameResultViewController : UIViewController
+
+@end
